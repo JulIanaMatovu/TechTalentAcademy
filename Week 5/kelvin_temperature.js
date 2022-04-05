db60acdb3890a97877acc103396d0edaef9e2ffa
Juliana Matovu <juliexmatovu@gmail.com>
2022-04-05 14:13:55 +0100

    added kelvin_temperature.js file; new week 5 folder

---
+++ Week 5/kelvin_temperature.js
@@ -0,0 +1,13 @@
+const KELVIN= 294;  //constant variable that represents 293 kelvin (degrees)
+console.log(KELVIN);
+
+let celsius = KELVIN - 273; //conversion: kelvin to celsius
+console.log(celsius); 
+
+let fahrenheit = celsius * (9/5) + 32; //conversion: celsius to fahrenheit; this var type allows reassignment
+console.log(fahrenheit);
+
+fahrenheit = Math.floor(celsius * (9/5) + 32);
+console.log(fahrenheit);
+console.log("The temperature is " + 
+fahrenheit + " degrees Fahrenheit.");
