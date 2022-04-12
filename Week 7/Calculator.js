fe5c5c1e32bd2e3ae0dfd4b192a1bd2f60958fb0
Juliana Matovu <juliexmatovu@gmail.com>
2022-04-12 23:59:37 +0100

    calculator added

---
+++ Week 7/Calculator.js
@@ -0,0 +1,19 @@
+let firstValue;
+let secondValue;
+let operation;
+
+function multiplication(firstVal, secondVal) {
+    return firstVal * secondVal;
+}
+
+function addition(firstVal, secondVal) {
+    return firstVal + secondVal;
+}
+
+function subtraction(firstVal, secondVal) {
+    return firstVal - secondVal;
+}
+
+function division(firstVal, secondVal) {
+    return firstVal / secondVal;
+}
